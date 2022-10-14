55ad2071faf49daedef25debcbbcae862988872b
Melikova <mhmelikova@gmail.com>
2022-10-14 15:32:34 +0400

    navbar fadein/fadeout animation added

--- public/assets/home.js
+++ public/assets/home.js
@@ -1,10 +1,19 @@
 $(window).scroll(function() {
     if ($(this).scrollTop()>250)
      {
-        $('#hidden_menu').slideDown("slow");
+        $('#hidden_menu').animate(
+            { opacity: 1 },
+            { queue: false, duration: 'normal' }
+           );
+        $('#hidden_menu').slideDown('slow')
      }
     else
      {
-      $('#hidden_menu').slideUp("slow");
+        $('#hidden_menu').slideUp('slow')
+        $('#hidden_menu').animate(
+         { opacity: 0.6 },
+         { queue: false, duration: 'normal' }
+        );
      }
  });
+
